ae3aa710cba19288db5f75fedc23542ecbe3ce40
EmilBjorkeng <emil.bjorkeng@gmail.com>
2025-03-07 18:48:47 +0100

    Add base64 to image convertion

--- image/script.js
+++ image/script.js
@@ -1,18 +1,27 @@
-const chooseFile = document.getElementById("choose-file");
-const imgPreview = document.getElementById("img-preview");
+const chooseFile = document.getElementById('choose-file');
+const imgPreview = document.getElementById('img-preview');
 const textBox = document.getElementById('text-box');
 
-chooseFile.addEventListener("change", () => {
+chooseFile.addEventListener('change', () => {
     const files = chooseFile.files[0];
     if (files) {
         const fileReader = new FileReader();
         fileReader.readAsDataURL(files);
-        fileReader.addEventListener("load", function () {
-            imgPreview.style.display = "block";
+        fileReader.addEventListener('load', function () {
+            imgPreview.style.display = 'block';
             imgPreview.innerHTML = '<img src="' + this.result + '" />';
 
             textBox.value = this.result;
         });
     }
 });
-textBox.value = "";
+textBox.value = '';
+
+textBox.addEventListener('input', (e) => {
+    if (textBox.value === '') {
+        imgPreview.style.display = 'none';
+        return;
+    }
+    imgPreview.style.display = 'block';
+    imgPreview.innerHTML = '<img src="' + textBox.value + '" />';
+});
